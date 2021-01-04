4968d3e9ffd41bb352bb2716208bea1a96ad7b6b
nwawre2 <Niyati.Wawre@ofwat.gov.uk>
2021-01-04 17:44:46 +0000

    Created 2 diff  PC_ODI_company and PC_ODI only

--- models/tables/D_PC_ODI_Company.sql
+++ models/tables/D_PC_ODI_Company.sql
@@ -10,10 +10,20 @@ D_ODI_form as (
 PCCompanyAMP as (
     select * from {{ ref('D_PC_Company_AMP') }}
 ),
+/*added company and element because more than 1 records of pc and odi 
+where getting pulled when they were not considered*/
+Company as (
+    select * from {{ ref('D_Company') }}
+),
+element as (
+    select * from {{ ref('D_Elements') }}
+),
 
 final as (
- select {{dbt_utils.hash(dbt_utils.concat(['PR14.Unique_ID','ODI_type.ODI_id','D_ODI_form.ODI_form_id']))}} [PC_ODI_id]
-	  , PR14.Unique_ID
+ select {{dbt_utils.hash(dbt_utils.concat(['Performance_commitment','Primary_Category','ODI_type.ODI_id','D_ODI_form.ODI_form_id','Company','element.Element_acronym']))}} [PC_ODI_id]
+	  , PR14.[Unique_ID]
+      , element.Element_acronym
+      , Company
       , Performance_commitment
       , Primary_Category
       , PR14.ODI_type
@@ -29,6 +39,10 @@ left join ODI_type on
  PR14.[ODI_type]=ODI_type.[ODI_name]
 left join D_ODI_form on
  PR14.[ODI_form]=D_ODI_form.[odi_form_name]
+left join Company on
+ PR14.[Company]=Company.[Company_name]
+left join element on
+ PR14.[Element_acronym]=element.Element_acronym
 )
 
 select * from final
