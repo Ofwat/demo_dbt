06e89428b3411fa98dc6c181f3d1c800561f3b29
arvsab-ofwat <arvind.sabharwal@ofwat.gov.uk>
2021-02-05 15:30:34 +0000

    merging in fact stg union

---
+++ models/staging/stg_F_PC_APR14_15_estimate.sql
@@ -0,0 +1,37 @@
+SELECT [Unique ID]
+,'2014-15' Year
+,'Estimate' Submission_Status
+      ,[Company type] Company_type
+      ,[Company] Company
+      ,[Element (price control)] Element_price_control
+      ,[Element (price control) acronym] Element_price_control_acronym
+      ,[Outcome]
+      ,[PC ref. (company)] PC_ref_company
+      ,[PR14 Annex 4 order]
+      ,[Performance commitment]
+      ,[ODI type]
+      ,[ODI form]
+      ,[In-period ODI]
+      ,[Standard ('vanilla') financial ODI] Standard_vanilla_financial_ODI
+      ,[Primary Category]
+      ,[PC unit]
+      ,[PC unit description]
+      ,[Decimal places]
+      ,[Direction of improving performance]
+,'N/A' notional_outperformance_payment_or_underperformance_payment_accrued
+,0 notional_outperformance_payment_or_underperformance_payment_accrued_GBPm
+,'N/A' outperformance_payment_or_underperformance_payment_in_period_ODI
+,0 outperformance_payment_or_underperformance_payment_in_period_ODI_GBPm
+,0 PCL
+,'NA' PCL_met
+,[2014-15 starting level (PR14 FD)] performance_level_actual
+,0 Total_AMP6_outperformance_payment_or_underperformance_payment_forecast
+,0 Total_AMP6_outperformance_payment_or_underperformance_payment_forecast_GBPm
+,0 Financial_ODI
+,0 UnderP_payment_collar
+,0 UnderP_payment_deadband
+,0 OutP_payment_deadband
+,0 OutP_payment_cap
+from PR14_FD_outcome_New_CSV
+
+
