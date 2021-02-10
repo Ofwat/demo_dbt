7f6a0308e87d33eb86b3f553d93d751d34eb0913
arvsab-ofwat <arvind.sabharwal@ofwat.gov.uk>
2021-02-10 15:31:53 +0000

    F_PC_APR schema name removed

--- models/tables/F_PC_APR.sql
+++ models/tables/F_PC_APR.sql
@@ -16,14 +16,14 @@ notional_outperformance_payment_or_underperformance_payment_accrued_GBPm Notiona
 ,[outperformance_payment_or_underperformance_payment_in_period_ODI_GBPm] Payment_In_Period_ODI_GBPm
 
 FROM arv_staging.[stg_F_PC_APR_UNION] list
-  left join arv.d_water_company d_co on list.Company=d_co.water_company_acronym
+  left join d_water_company d_co on list.Company=d_co.water_company_acronym
 
-left join nw.D_PC_Company_AMP DPC on list.[Unique ID]=DPC.Unique_ID
+left join D_PC_Company_AMP DPC on list.[Unique ID]=DPC.Unique_ID
 
-left join [arv].[D_odi_Type] ODI_Type on list.[notional_outperformance_payment_or_underperformance_payment_accrued]=ODI_Type.ODI_Type_Description
-left join [arv].[D_odi_Type] ODI_Type_INP_ODI on list.[outperformance_payment_or_underperformance_payment_in_period_ODI]=ODI_Type_INP_ODI.ODI_Type_Description
-left join [arv].[D_element] e on list.[Element_price_control_acronym]=e.Element_acronym
+left join [D_odi_Type] ODI_Type on list.[notional_outperformance_payment_or_underperformance_payment_accrued]=ODI_Type.ODI_Type_Description
+left join [D_odi_Type] ODI_Type_INP_ODI on list.[outperformance_payment_or_underperformance_payment_in_period_ODI]=ODI_Type_INP_ODI.ODI_Type_Description
+left join [D_element] e on list.[Element_price_control_acronym]=e.Element_acronym
 
-left join  [arv].[D_ODI_Form] form on list.[ODI form]=form.[odi_form_name]
-left join  arv.D_Submission_status Submission on list.[Submission_Status]=replace(Submission.submission_status_description,'APR','')
+left join  [D_ODI_Form] form on list.[ODI form]=form.[odi_form_name]
+left join  D_Submission_status Submission on list.[Submission_Status]=replace(Submission.submission_status_description,'APR','')
 where list.Company is not null
