ed9a06d50d3eacf2412d1a4b1ab723e273c9c311
nwawre2 <Niyati.Wawre@ofwat.gov.uk>
2021-03-02 15:24:20 +0000

    adding 2018-19 in union and making changes in financial incentive one

--- models/staging/stg_F_pc_apr_union.sql
+++ models/staging/stg_F_pc_apr_union.sql
@@ -8,6 +8,8 @@ select * from [stg_F_pc_apr_16_17]
 union
 select * from [stg_F_pc_apr_17_18]
 union
+select * from [stg_F_pc_apr_18_19]
+union
 select * from [stg_F_pc_apr_19_20_estimate]
 union 
 select * from [stg_F_pc_apr_19_20]
--- models/tables/F_PC_apr.sql
+++ models/tables/F_PC_apr.sql
@@ -7,10 +7,7 @@ company as (
 pccompamp as (
     select * from {{ ref('D_PC_company_amp') }}
 ),
-notionalincentive as (
-    select * from {{ ref('D_Financial_incentive_type') }}
-),
-inpincentive as (
+financialincentive as (
     select * from {{ ref('D_Financial_incentive_type') }}
 ),
 element as (
@@ -42,27 +39,29 @@ renamed as (
     ,pcl 
     ,pcl_met
     ,performance_level_actual pcl_Actual
-    ,coalesce(notionalincentive.Financial_incentive_type_id,inpincentive.Financial_incentive_type_id ) Financial_incentive_type_id
     ,isnumeric(performance_level_actual) numeric_pcl_actual
     ,case when isnumeric(performance_level_actual) =1 then performance_level_actual 
     else null end numeric_derived_pcl_actual
-    -- need to confirm from Anne
-	,Total_AMP6_outperformance_payment_or_underperformance_payment_forecast
+    ,financialincentive.financial_incentive_id
+    ,coalesce([notional_outperformance_payment_or_underperformance_payment_accrued_GBPm],
+    [outperformance_payment_or_underperformance_payment_in_period_ODI_GBPm]) financial_incentive_payment_GBPm
+    ,Total_AMP6_outperformance_payment_or_underperformance_payment_forecast
     ,Total_AMP6_outperformance_payment_or_underperformance_payment_forecast_GBPm
-    ,notionalincentive.Financial_incentive_type_id notional_incentive_type_id
-    ,notional_outperformance_payment_or_underperformance_payment_accrued_GBPm notional_incentive_payment_accrued_GBPm
-    ,inpincentive.Financial_incentive_type_id In_Period_incentive_type_id
-    ,outperformance_payment_or_underperformance_payment_in_period_ODI_GBPm In_Period_payment_GBPm
     from Fpcaprunion 
         left join company  on Fpcaprunion.company=company.water_company_acronym
         left join pccompamp on Fpcaprunion.unique_id=pccompamp.unique_id
         left join element on Fpcaprunion.element_acronym=element.element_acronym
-        left join submission on Fpcaprunion.submission_status=replace(submission.submission_status_description,'APR','')
-        left join notionalincentive on Fpcaprunion.notional_outperformance_payment_or_underperformance_payment_accrued=notionalincentive.financial_incentive_type_desc
-        left join inpincentive on Fpcaprunion.outperformance_payment_or_underperformance_payment_in_period_ODI=inpincentive.financial_incentive_type_desc
+        left join submission on Fpcaprunion.submission_status=submission.submission_status_name
+        left join financialincentive on (coalesce([notional_outperformance_payment_or_underperformance_payment_accrued],
+                            [outperformance_payment_or_underperformance_payment_in_period_ODI])) = financialincentive.financial_incentive_type
+                    and 
+                (case when [notional_outperformance_payment_or_underperformance_payment_accrued] is not null then 'Notional Period'
+                when [outperformance_payment_or_underperformance_payment_in_period_ODI] is not null then 'IN Period ODI'
+                else null end) = financialincentive.Incentive_Period
         left join odi_form on Fpcaprunion.odi_form = odi_form.odi_form_name
         left join odi_type on Fpcaprunion.odi_type = odi_type.odi_type_name
         where Fpcaprunion.company is not null
 )
 
 select * from renamed
+
